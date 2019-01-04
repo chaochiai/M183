c3cb331f638f7e1f9ed524ed7e3ace04b6b6dbc6
cochiai <chantalochiaiit@gmail.com>
2019-01-04 20:37:30 +0100

    encryption tasks

---
+++ M183/Controllers/EncryptionCesarCipherController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M183.Controllers
+{
+    public class EncryptionCesarCipherController : Controller
+    {
+        // GET: EncryptionCesarCipher
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ M183/Controllers/EncryptionOneTimePadController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M183.Controllers
+{
+    public class EncryptionOneTimePadController : Controller
+    {
+        // GET: EncryptionOneTimePad
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
